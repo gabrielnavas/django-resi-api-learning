4f0e94427c00982aa08c60f3cbdd71bfa30693b3
Gabriel Navas <gabnanavas.dev@gmail.com>
2023-02-11 20:33:29 -0300

    param query, body and headers example request

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,11 @@
 import pprint
 import requests
 
-endpoint = "http://127.0.0.1:8000/"
+endpoint = "http://127.0.0.1:8000/api?a=2"
 
-response = requests.get(endpoint)
-pprint.pprint(response.content)
+response = requests.get(
+    endpoint, 
+    params={"abc": 123}, 
+    json={"query": "hello"}
+)
+pprint.pprint(response.json())
