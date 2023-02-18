d147a54244442356ac73c1b6970e5cdd74152121
Gabriel Navas <gabnanavas.dev@gmail.com>
2023-02-18 20:01:12 -0300

    try get data from proprety model

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,11 +1,9 @@
 import pprint
 import requests
 
-endpoint = "http://127.0.0.1:8000/api?a=2"
+endpoint = "http://127.0.0.1:8000/api"
 
 response = requests.get(
     endpoint, 
-    params={"abc": 123}, 
-    json={"query": "hello"}
 )
 pprint.pprint(response.json())
